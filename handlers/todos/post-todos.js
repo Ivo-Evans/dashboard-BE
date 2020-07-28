c3b99c6c5a784ae677015198ca56f0ea1ba0239b
Ivo Evans <ievans.storrie@gmail.com>
2020-07-28 18:24:26 +0100

    feat(todo handlers): add error handling

--- handlers/todos/post-todos.js
+++ handlers/todos/post-todos.js
@@ -2,6 +2,11 @@ const todoModels = require("../../db/models/todos.js")
 
 function postTodo(req, res, next) {
     const { users_id, body } = req
+    if (!body.todo) {
+        const err = new Error("todo content is required.")
+        err.code = 400
+        return next(err)
+    }
     todoModels.postTodo({...body, users_id})
         .then(() => {
             res.status(200).send()
--- handlers/todos/put-todos.js
+++ handlers/todos/put-todos.js
@@ -1,7 +1,14 @@
 const todoModels = require("../../db/models/todos.js")
 
 function putTodo(req, res, next) {
+    // add error handling
     const { users_id, body } = req
+    const {id, todo, completed} = body
+    if (!(id && todo && completed)) {
+        const err = new Error("id, todo content and completed status are required.")
+        err.code = 400
+        return next(err)
+    }
     todoModels.putTodo({...body, users_id})
         .then(() => {
             res.status(200).send()
