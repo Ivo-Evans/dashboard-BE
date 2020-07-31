f9abdc32ce55dd1c4e747dbc460ca62ff0ec272a
Ivo Evans <ievans.storrie@gmail.com>
2020-07-31 18:30:29 +0100

    feat(todo): allow posting an empty todo which returns the new todo

--- handlers/todos/post-todos.js
+++ handlers/todos/post-todos.js
@@ -2,14 +2,14 @@ const todoModels = require("../../db/models/todos.js")
 
 function postTodo(req, res, next) {
     const { users_id, body } = req
-    if (!body.todo) {
-        const err = new Error("todo content is required.")
-        err.code = 400
-        return next(err)
-    }
+    // if (!body.todo) {
+    //     const err = new Error("todo content is required.")
+    //     err.code = 400
+    //     return next(err)
+    // }
     todoModels.postTodo({...body, users_id})
-        .then(() => {
-            res.status(200).send()
+        .then((result) => {
+            res.status(200).send(result.rows[0])
         })
         .catch(next)
 }
--- handlers/todos/put-todos.js
+++ handlers/todos/put-todos.js
@@ -3,7 +3,7 @@ const todoModels = require("../../db/models/todos.js")
 function putTodo(req, res, next) {
     const { users_id, body } = req
     const {id, todo, completed} = body
-    if (!(id && todo && completed)) {
+    if (!(id && todo && typeof completed === "boolean")) {
         const err = new Error("id, todo content and completed status are required.")
         err.code = 400
         return next(err)
