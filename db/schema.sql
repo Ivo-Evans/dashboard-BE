4678a4cefe533621474d646509317e802d35ac2d
Ivo Evans <ievans.storrie@gmail.com>
2020-08-01 09:44:10 +0100

    feat(db): add teams and warmers to schema. Add a script to load warmers to db

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,35 @@
 BEGIN;
 
-    DROP TABLE IF EXISTS users, todos, photos CASCADE;
+    DROP TABLE IF EXISTS teams, users, warmers, todos, photos CASCADE;
+
+    CREATE TABLE teams
+    (
+        id SERIAL PRIMARY KEY,
+        team VARCHAR(255) NOT NULL UNIQUE
+    );
 
     CREATE TABLE users
     (
         id SERIAL PRIMARY KEY,
   	    username VARCHAR(255) NOT NULL UNIQUE,
 	    email VARCHAR(255) NOT NULL UNIQUE,
-	    password_slug VARCHAR(255) NOT NULL
+	    password_slug VARCHAR(255) NOT NULL,
+        teams_id INTEGER REFERENCES teams(id)
+    );
+
+    CREATE TABLE warmers
+    (
+        id SERIAL PRIMARY KEY,
+        warmer VARCHAR(255) NOT NULL,
+        date VARCHAR(255) NOT NULL,
+        users_id INTEGER REFERENCES users(id)
     );
 
     CREATE TABLE todos
     (
         id SERIAL PRIMARY KEY,
         users_id INTEGER REFERENCES users(id),
-        todo TEXT NOT NULL,
+        todo TEXT,
         completed BOOLEAN
     );
 
